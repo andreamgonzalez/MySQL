df1020442b55fe12e52a3396d2beda23d9090b40
Andrea Michelle <andreagon6135@gmail.com>
2022-10-02 14:01:13 -0400

    Update outer_space.sql

--- sql-ddl-design/outer_space.sql
+++ sql-ddl-design/outer_space.sql
@@ -13,16 +13,34 @@ CREATE TABLE planets
   name TEXT NOT NULL,
   orbital_period_in_years FLOAT NOT NULL,
   orbits_around TEXT NOT NULL,
-  galaxy TEXT NOT NULL,
+  galaxy INTEGER REFERENCES galaxies(id) NOT NULL,
   moons TEXT[]
 );
+
+
+CREATE TABLE galaxies
+(
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+);
+
+CREATE TABLE moons
+(
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+);
+
+INSERT INTO galaxies
+  (name)
+VALUES
+  ('Milky Way')
 
 INSERT INTO planets
   (name, orbital_period_in_years, orbits_around, galaxy, moons)
 VALUES
-  ('Earth', 1.00, 'The Sun', 'Milky Way', '{"The Moon"}'),
-  ('Mars', 1.88, 'The Sun', 'Milky Way', '{"Phobos", "Deimos"}'),
-  ('Venus', 0.62, 'The Sun', 'Milky Way', '{}'),
-  ('Neptune', 164.8, 'The Sun', 'Milky Way', '{"Naiad", "Thalassa", "Despina", "Galatea", "Larissa", "S/2004 N 1", "Proteus", "Triton", "Nereid", "Halimede", "Sao", "Laomedeia", "Psamathe", "Neso"}'),
-  ('Proxima Centauri b', 0.03, 'Proxima Centauri', 'Milky Way', '{}'),
-  ('Gliese 876 b', 0.23, 'Gliese 876', 'Milky Way', '{}');
+  ('Earth', 1.00, 'The Sun', 1, '{"The Moon"}'),
+  ('Mars', 1.88, 'The Sun', 1, '{"Phobos", "Deimos"}'),
+  ('Venus', 0.62, 'The Sun', 1, '{}'),
+  ('Neptune', 164.8, 'The Sun', 1, '{"Naiad", "Thalassa", "Despina", "Galatea", "Larissa", "S/2004 N 1", "Proteus", "Triton", "Nereid", "Halimede", "Sao", "Laomedeia", "Psamathe", "Neso"}'),
+  ('Proxima Centauri b', 0.03, 'Proxima Centauri', 1, '{}'),
+  ('Gliese 876 b', 0.23, 'Gliese 876', 1, '{}');
